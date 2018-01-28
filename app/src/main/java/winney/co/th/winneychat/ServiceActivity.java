2a9a301cf05411cef12ff220ca71e7a763f65677
winney <thammarath014@gmail.com>
2018-01-28 14:18:36 +0700

    Add ChatFragment Success.

--- app/src/main/java/winney/co/th/winneychat/ServiceActivity.java
+++ app/src/main/java/winney/co/th/winneychat/ServiceActivity.java
@@ -3,12 +3,22 @@ package winney.co.th.winneychat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import winney.co.th.winneychat.fragment.ChatFragment;
+import winney.co.th.winneychat.fragment.RegisterFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentServiceFragment, new ChatFragment())
+                    .commit();
+        }
     }// Main Method
 
 
