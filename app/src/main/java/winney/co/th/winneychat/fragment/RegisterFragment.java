f38ab697b303b7613e059203ef079cdc285d34a2
winney <thammarath014@gmail.com>
2018-01-28 10:16:53 +0700

    Auth by Email Success.

--- app/src/main/java/winney/co/th/winneychat/fragment/RegisterFragment.java
+++ app/src/main/java/winney/co/th/winneychat/fragment/RegisterFragment.java
@@ -1,6 +1,7 @@
 package winney.co.th.winneychat.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import winney.co.th.winneychat.MainActivity;
 import winney.co.th.winneychat.R;
@@ -90,8 +97,30 @@ public class RegisterFragment extends Fragment {
             alertUtil.normalDialog("Have Space", "Please filled All Every Blank");
         } else {
             // No Space
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
-        }
+            firebaseAuth.createUserWithEmailAndPassword(emilString, passwordString)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(), "Regeter Success",
+                                        Toast.LENGTH_SHORT).show();
+                                getActivity().getSupportFragmentManager().popBackStack();
+
+                            } else {
+                                AlertUtil alertUtil = new AlertUtil(getActivity());
+                                alertUtil.normalDialog("Cannot Register", task.getException().getMessage());
+                            }
+
+                        }// onComplete
+                    });
+
+
+
+
+        } // End IF
 
     }
 
